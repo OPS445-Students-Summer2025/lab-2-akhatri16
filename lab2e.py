76cb2d84cc053a8cee9b8497fb79143197d967e9
abhisek khatri <akhatri16@myseneca.ca>
2025-05-19 17:15:30 -0400

    Final Lab 2 submission

---
+++ lab2e.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# Author: abhisek khatri
+# Author ID: akhanal16
+# Date Created: 2025/05/18
+
+timer = 10
+while timer != 0:
+    print(timer)
+    timer -= 1
+print("blast off!")
---
+++ lab2g.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# Author: abhisek khatri
+# Author ID: akhanal16
+# Date Created: 2025/05/18
+
+import sys
+
+if len(sys.argv) == 2:
+    timer = int(sys.argv[1])
+else:
+    timer = 3
+
+while timer != 0:
+    print(timer)
+    timer -= 1
+print("blast off!")
